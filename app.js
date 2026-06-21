75cfb364fcf380062427b4c28f626e3778a5f0e8
Bryan Dailey <bryan.dailey@snhu.edu>
2026-06-21 10:31:43 -0400

    Module 7 completed baseline

--- app.js
+++ app.js
@@ -12,8 +12,14 @@ var apiRouter = require('./app_api/routes/index');
 
 var handlebars = require('hbs');
 
+// Wire in our authentication module
+var passport = require('passport');
+require('./app_api/config/passport');
+
 // Bring in the database
 require('./app_api/models/db');
+// Bring in the .env file
+require('dotenv').config();
 
 var app = express();
 
@@ -30,11 +36,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
 
 // Enable CORS
 app.use('/api', (req, res, next)=> {
   res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
@@ -48,6 +55,15 @@ app.use('/api', apiRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
+});
+
+// Catch unauthorized error and create 401
+app.use((err, req, res, next) => {
+  if(err.name === 'UnauthorizedError') {
+    res
+      .status(401)
+      .json({"message": err.name + ": " + err.message});
+  }
 });
 
 // error handler
--- app_admin/src/app/trip-listing/trip-listing.component.ts
+++ app_admin/src/app/trip-listing/trip-listing.component.ts
@@ -7,6 +7,8 @@ import { Trip } from '../models/trip';
 
 import { Router } from '@angular/router';
 
+import { AuthenticationService } from '../services/authentication.service';
+
 @Component({
   selector: 'app-trip-listing',
   standalone: true,
@@ -23,7 +25,8 @@ export class TripListingComponent implements OnInit {
     
   constructor(
     private tripDataService: TripDataService,
-    private router: Router
+    private router: Router,
+    private authenticationService: AuthenticationService
   ) {
     console.log('trip-listing constructor');
   }
@@ -54,6 +57,11 @@ export class TripListingComponent implements OnInit {
   ngOnInit(): void {
     console.log('ngOnInit');
     this.getStuff();
+  }
+
+  public isLoggedIn()
+  {
+    return this.authenticationService.isLoggedIn();
   }
 
 }
